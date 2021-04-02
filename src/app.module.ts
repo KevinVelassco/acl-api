0771fb8fd6cde2c48bde18ba9709db122b389c84
KevinVelassco <kevinvelassco@gmail.com>
2021-04-02 14:39:42 -0500

    feat: assigned-roles module is added

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,6 +11,7 @@ import { ParametersModule } from './modules/parameters/parameters.module';
 import { UsersModule } from './modules/users/users.module';
 import { CompaniesModule } from './modules/companies/companies.module';
 import { RolesModule } from './modules/roles/roles.module';
+import { AssignedRolesModule } from './modules/assigned-roles/assigned-roles.module';
 
 import appConfig from './config/app.config';
 import appConfigSchema from './config/app.config.schema';
@@ -49,7 +50,8 @@ const envPath = path.resolve(__dirname, `../.env.${NODE_ENV}`);
     ParametersModule,
     UsersModule,
     CompaniesModule,
-    RolesModule
+    RolesModule,
+    AssignedRolesModule
   ],
   controllers: [AppController],
   providers: [AppService]
